9c59a6ebcee0bf14a9323cceba299aa52447baf0
kyj2212 <kyj2212@gmail.com>
2022-07-19 11:41:18 +0900

    pause : compare at directly and at call method

--- src/main/java/org/example/LionClass/ch36/ExceptionEx02.java
+++ src/main/java/org/example/LionClass/ch36/ExceptionEx02.java
@@ -1,4 +1,12 @@
 package org.example.LionClass.ch36;
 
+import java.io.File;
+
 public class ExceptionEx02 {
+
+
+    static File createFile(String fileName){
+
+        return new File(fileName);
+    }
 }
