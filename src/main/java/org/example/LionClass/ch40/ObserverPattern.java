ea60c8c326f7889b71eee580b6c830eb566df1e8
kyj2212 <kyj2212@gmail.com>
2022-07-21 11:08:36 +0900

    FEAT:observer pattern, no use interface, instanceof

---
+++ src/main/java/org/example/LionClass/ch40/ObserverPattern.java
@@ -0,0 +1,116 @@
+package org.example.LionClass.ch40;
+
+
+public class ObserverPattern {
+
+
+
+    public static void main(String[] args) {
+        sol01();
+        System.out.println("=========");
+        sol02();
+    }
+
+
+    static void sol01(){
+        /*
+        문제 : 사람 객체의 `안녕`이 `button.click();`에 의해 실행되도록 해주세요.
+        조건 : Button에는 `사람` 이라는 언급이 없어야 합니다.
+        */
+        Button button = new Button();
+        button.setListener(new Human());
+        button.click();
+    }
+
+
+    interface Listener{
+        public void listen(String msg);
+    }
+
+    static class Button{
+
+        private Listener listener;
+        private Object obj;
+        private String mode;
+
+        void setListener(Listener listener){
+            this.listener=listener;
+        }
+
+        void click(){
+            listener.listen("버튼 click");
+        }
+
+        public void setClickEventListener(Cat cat) {
+            this.obj=cat;
+            mode="cat";
+        }
+        public void setClickEventListener(Dog dog) {
+            this.obj=dog;
+            mode="dog";
+        }
+        public void setClickEventListener(Owl owl) {
+            this.obj=owl;
+            mode="owl";
+        }
+
+        void fireClick(){
+            if(mode.equals("cat"))
+                ((Cat)obj).getClickEvent();
+            else if(mode.equals("dog"))
+                ((Dog)obj).getClickEvent();
+            else if(mode.equals("owl"))
+                ((Owl)obj).getClickEvent();
+        }
+    }
+    private static class Cat {
+        public void getClickEvent(){
+            System.out.println("고양이는 방금 버튼 클릭되었다는 사실을 전달받았습니다.");
+        }
+    }
+    private static class Dog  {
+        public void getClickEvent(){
+            System.out.println("강아지는 방금 버튼 클릭되었다는 사실을 전달받았습니다.");
+        }
+    }
+    private static class Owl {
+        public void getClickEvent(){
+            System.out.println("부엉이는 방금 버튼 클릭되었다는 사실을 전달받았습니다.");
+        }
+    }
+
+    static class Human implements Listener{
+        public void hi() {
+            System.out.println("Main::안녕!");
+        }
+
+        @Override
+        public void listen(String msg) {
+            hi();
+        }
+    }
+
+    static void sol02(){
+
+            // no use 상속, 구현
+            // 버튼 클래스는 모든 객체에게 클릭 사실을 알릴 수 있어야 한다.
+
+            Button aButton = new Button();
+            aButton.setClickEventListener(new Cat());
+            aButton.fireClick();
+            // 나(고양이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+
+            aButton.setClickEventListener(new Dog());
+            aButton.fireClick();
+            // 나(개)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+
+            aButton.setClickEventListener(new Owl());
+            aButton.fireClick();
+        // 나(부엉이)는 방금 버튼이 클릭되었다는 사실을 전달 받았습니다.
+
+
+    }
+
+
+
+}
