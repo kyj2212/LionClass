65d4fea5a80b9f1da2b1e079eb49592eca8c5b7d
kyj2212 <kyj2212@gmail.com>
2022-07-26 10:21:54 +0900

    Fix: inner anonymous class, method 2

--- src/main/java/org/example/LionClass/ch42/ButtonObserver.java
+++ src/main/java/org/example/LionClass/ch42/ButtonObserver.java
@@ -8,39 +8,42 @@ public class ButtonObserver {
     public static void main(String[] args) {
 
         View aButton = new Button();
-        aButton.addOnClickListener((msg)-> System.out.println("홍길동이 받은 메세지 : " + msg));
+        aButton.addOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(String msg) {
+                System.out.println("홍길동이 받은 메세지 : " + msg);
+            }
+
+            public void breath() {
+                System.out.println("홍길동 : 숨쉬다.");
+            }
+        });
         aButton.addOnClickListener((msg)-> System.out.println("홍길순이 받은 메세지 : " + msg));
-        aButton.fireClickEvent();
+        aButton.fireClickEvent(); // 홍길순, 홍길동 모두 클릭이벤트를 받아야함.
         aButton.fireClickEvent();
     }
 
 }
 class View {
-    List<OnClickListener> onClickListenerList;
+    private List<OnClickListener> onClickListeners;
 
-    View(){
-        onClickListenerList=new ArrayList<>();
-    }
+    public View(){onClickListeners=new ArrayList<>();}
 
     public void addOnClickListener(OnClickListener onClickListener) {
-        this.onClickListenerList.add(onClickListener);
+        this.onClickListeners.add(onClickListener);
     }
 
     public void fireClickEvent(){
         System.out.println("뷰가 클릭되었습니다.");
-        for(OnClickListener onClickListener : onClickListenerList)
+        for(OnClickListener onClickListener : onClickListeners)
             onClickListener.onClick("클릭");
     }
 
 }
 
-class Button extends View {
+class Button extends View { }
 
-}
-
-interface OnClickListener {
-    public void onClick(String msg);
-}
+interface OnClickListener { public void onClick(String msg);}
 
 /*class HongBoy implements OnClickListener {
     @Override
